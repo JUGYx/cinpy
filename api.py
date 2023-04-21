b3a15f73d1276b4a6d2257629ec297f557016afa
JUGYx <hellowo038@gmail.com>
2023-04-21 14:07:07 +0300

    api_url

--- api.py
+++ api.py
@@ -1,14 +1,15 @@
-import sys
 import requests
 import json
 from utils import *
 
-Headers = {"user-agent":"Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0"}
+headers = {"user-agent":"Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0"}
+
+api_url = "https://cinemana.shabakaty.com/api/android"
 
 def get(url, stream=False):
     try:
         with requests.session() as s:
-            response = s.get(url, headers=Headers, stream=stream)
+            response = s.get(url, headers=headers, stream=stream)
             if response.ok:
                 return response
             else:
@@ -17,13 +18,13 @@ def get(url, stream=False):
         die(-1, "\nHTTP request failed, check your network connection.\n")
 
 def look(query, page_number, genre):
-    url = f"https://cinemana.shabakaty.com/api/android/AdvancedSearch?level=0&videoTitle={query}&staffTitle={query}&type={genre}&page={page_number}&="
+    url = f"{api_url}/AdvancedSearch?level=0&videoTitle={query}&staffTitle={query}&type={genre}&page={page_number}&="
     response = get(url)
     result = json.loads(response.text)
     return result
 
 def season(show):
-    url = "https://cinemana.shabakaty.com/api/android/videoSeason/id/"+show['nb']
+    url = f"{api_url}/videoSeason/id/"+show['nb']
     response = get(url)
     result = json.loads(response.text)
     seasons={}
@@ -34,8 +35,8 @@ def season(show):
     return seasons
 
 def media(data):
-    subtitles_response = get("https://cinemana.shabakaty.com/api/android/allVideoInfo/id/"+data['nb'])
-    videos_response = get("https://cinemana.shabakaty.com/api/android/transcoddedFiles/id/"+data['nb'])
+    subtitles_response = get(f"{api_url}/allVideoInfo/id/"+data['nb'])
+    videos_response = get(f"{api_url}/transcoddedFiles/id/"+data['nb'])
    
     videos_result = json.loads(videos_response.text)
     subtitles_result = json.loads(subtitles_response.text)
